913ec4f2122a5b503dc56c49dbe4651e489448d5
Rahul Sharma <122264642+RahulProjects01@users.noreply.github.com>
2024-07-10 11:33:57 +0530

    Update Contect.jsx
    token

--- src/components/Contect/Contect.jsx
+++ src/components/Contect/Contect.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import './Contect.css';
 
 const Contect = () => {
+   const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "dc9dfe8b-b452-4375-b532-1871c8a0166f");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div className='Contect-page' id='contact'>
       <h1>Let's connect</h1>
-      <form action="https://api.web3forms.com/submit" method="POST">
-        <input type="hidden" name="access_key" value="dc9dfe8b-b452-4375-b532-1871c8a0166f" />
+      <form onSubmit={onSubmit}>
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" name="name" placeholder='Enter Name' required />
         <label htmlFor="email">Email:</label>
@@ -15,6 +38,7 @@ const Contect = () => {
         <textarea id="message" name="message" rows="4" placeholder='Write a message....' required></textarea>
         <button className='su' type="submit">Submit</button>
       </form>
+            <span>{result}</span>
     </div>
   )
 }
