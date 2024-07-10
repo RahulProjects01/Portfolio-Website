099be57489e5c12ad26002ec2dadbfd62d509b73
Rahul Sharma <122264642+RahulProjects01@users.noreply.github.com>
2024-07-10 11:44:44 +0530

    Update MyHomePage.jsx
    Profile img change

--- src/components/MyHomePage/MyHomePage.jsx
+++ src/components/MyHomePage/MyHomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './MyHomePage.css'
-import logo from "../../assets/images/WhatsApp%20Image%202024-07-06%20at%2012.51.38%20PM.jpeg"
+import logo from "../../assets/images/Profile.jpg"
 
 const MyHomePage = () => {
     return (
